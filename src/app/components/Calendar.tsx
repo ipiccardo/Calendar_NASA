74481a40011855092e9ce64d349a84663c62d5f1
Nuevo Nombre de Usuario <ivan.piccardo@tsoftlatam.com>
2024-05-07 15:29:44 -0300

    Se arma la estructura del calendario

--- src/app/components/Calendar.tsx
+++ src/app/components/Calendar.tsx
@@ -6,42 +6,57 @@ import { Pictures } from '../../../types'
 
 export default async function Calendar() {
 
+    const { currentDay, currentMonth, currentYear, currentMonthName, daysInCurrentMonth } = getCurrentMonthData()
 
     // Sacar esto a una función que siempre me traiga el inicio de mes, el fin de mes, el current mes, la cantidad de dias del mes
     // ---------------------------------------------------------
-    const startDay = '2024-05-01'
-    const endDay = '2024-05-07'
+    const startDay = `${currentYear}-${currentMonth}-01`
+    const amountOfDays = `${currentYear}-${currentMonth}-${daysInCurrentMonth}`
+    const endDay = `${currentYear}-${currentMonth}-${currentDay}`
 
 
     const allPictures = await api.list(startDay, endDay)
 
 
-    const totalDaysOfMonth = [
-        ...allPictures,
-        {
-            date: parseInt(endDay) < parseInt('2024-05-31') ? parseInt(endDay) + 1 : '',
-            explanation: "",
-            hdurl: '',
-            media_type: '',
-            service_version: '',
-            title: '',
-            url: '/default.jpg'
-        }
-    ]
+    const totalDaysOfMonth: Pictures[] = [];
+    for (let i = 1; i <= daysInCurrentMonth; i++) {
+        const currentDate = `${currentYear}-${currentMonth}-${i <= 9 ? '0' + i : i}`;
+        const picture = allPictures.find((p: Pictures) => p.date === currentDate.toString());
 
-    console.log(totalDaysOfMonth, 'totla dias en el mes')
+        picture ? totalDaysOfMonth?.push(picture) :
+
+            totalDaysOfMonth.push({
+                title: '',
+                date: currentDate,
+                explanation: "",
+                media_type: '',
+                thumbnailUrl: '',
+                url: '/default.jpg'
+            });
+
+    }
+
 
     return (
         <>
             {
-                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-6 lg:gap-8 w-full py-8 px-8 ">
-                    {allPictures?.map((picture: Pictures, i: number) => {
-                        return (
-                            <div key={picture.title} className={`h-80 border-2 border-solid border-white rounded`}>
-                                <Image className='h-full w-full overflow-hidden aspect-square' width={200} height={200} src={`${picture.media_type === 'image' ? picture.url : '/default.jpg'}`} alt="imagen" />
-                            </div>
-                        );
-                    })}
+                <div>
+                    <div>
+                        <h1 className='text-center'>MAYO</h1>
+                        <div className='flex justify-between'>
+                            <button>Next</button>
+                            <button>Prev</button>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-6 lg:gap-8 w-full py-8 px-8 ">
+                        {totalDaysOfMonth?.map((picture: Pictures, i: number) => {
+                            return (
+                                <div key={currentDay + i} className={`h-80 border-2 border-solid border-white rounded`}>
+                                    <Image style={{ objectFit: "cover" }} loading='lazy' className='h-full w-full overflow-hidden aspect-square' width={500} height={500} src={`${picture.media_type === 'image' ? picture.url : '/default.jpg'}`} alt="imagen" />
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             }
         </>
